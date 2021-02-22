d4200715dcaae106f21cfe06bb400334e34d83a5
Luis Sanz Goizueta <ataraxa@hotmail.com>
2021-02-22 19:38:31 +0100

    Add files via upload

--- ProgramacionC/main.c
+++ ProgramacionC/main.c
@@ -3,6 +3,9 @@
 #include<string.h>
 #define RUTA "T:\\agenda\\agenda.csv"
 #define SEPARADOR ","
+#define GRABAR 1
+#define LISTAR 2
+#define SALIR 3
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 struct contacto
 {
@@ -27,7 +30,7 @@ Contacto pedirDatosContacto()
 }
 void imprimirContacto(Contacto c)
 {
-	printf("NOMBRE: %s ****** TELÉFONO: %s", c.nombre, c.telefono);
+	//printf("NOMBRE: %s ****** TELÉFONO: %s", c.nombre, c.telefono);
 }
 void grabarContacto (Contacto c)
 {
@@ -38,29 +41,66 @@ void grabarContacto (Contacto c)
 	fputs(c.nombre, filePtr);
 	fclose(filePtr);
 }
-int main(int argc, char *argv[]) {
 
-//FILE *filePtr=fopen(RUTA, "w");
-/*Contacto c=pedirDatosContacto();
-grabarContacto(c);
-imprimirContacto(c);*/
-leerContactos();
-}
 void leerContactos()
 {
+	Contacto* c=(Contacto*)malloc(sizeof(Contacto));
+	
+	c->nombre=(char*)malloc(sizeof(char));
+	c->telefono=(char*)malloc(sizeof(char));
 	FILE *filePtr=fopen(RUTA, "r");
 	//1-Un while que busque el EOF
 	//2-Dentro, uno que busque la coma y el segundo, un salto de línea.
 	//Usa strtok coño!!
 	
 	char* linea=(char*)malloc(sizeof(char));
-	while (linea!=NULL)
+	char* token=(char*)malloc(sizeof(char));
+	linea=fgets(linea, 20, filePtr);
+	while(linea!=NULL)
 	{
-	linea=fgets(linea, 20, filePtr);	
-	printf("LINEA LEIDA: %s", linea);
+		
+		token = strtok(linea, SEPARADOR);//saco el nombre
+		strcpy(c->nombre,token);
+		token=strtok(NULL, SEPARADOR);
+		strcpy(c->telefono, token);
+		printf("NOMBRE: %s TELEFONO: %s\n", c->nombre, c->telefono);
+		linea=fgets(linea, 20, filePtr);
+		
 	}
 	
 	
 	fclose(filePtr);
+	free(c);
+
+}
+int mostrarMenu()
+{
+	printf("INTRODUZCA LA OPCIÓN: \n");
+	printf("%d Grabar nuevo contacto \n", GRABAR)	;
+	printf("%d Listar contactos \n", LISTAR)	;
+	printf("%d SALIR \n", SALIR)	;
+	int opcion=0;
+	scanf("%d", &opcion);
+	return opcion;
+}
+int main(int argc, char *argv[]) {
+int opcion=mostrarMenu();
+while(opcion!=SALIR)
+{
+	switch(opcion)
+	{
+		case GRABAR: ;
+		Contacto c=pedirDatosContacto();
+			grabarContacto(c);
+		break;
+		case LISTAR:
+			leerContactos();
+			break;
+			
+				
+	}
+	opcion=mostrarMenu();
 }
 
+
+}
